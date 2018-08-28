8574eec83d3b3d1a51d308ec98cedad858fa279c
dyliman <dyyliman@gmail.com>
2018-08-28 15:01:03 -0700

    Reducers Tests Passed

--- src/actions/search.js
+++ src/actions/search.js
@@ -2,7 +2,6 @@ import searchYouTube from '../lib/searchYouTube.js';
 import changeVideoList from './videoList.js';
 import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
-import ReduxThunk from 'redux-thunk';
 
 var handleVideoSearch = (q) => {
   return searchYouTube;
