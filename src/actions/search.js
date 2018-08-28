337d8336a415c5c1d255c1dbcf08c7dd298733ae
dyliman <dyyliman@gmail.com>
2018-08-28 14:42:02 -0700

    Action Tests Passed

--- src/actions/search.js
+++ src/actions/search.js
@@ -2,10 +2,10 @@ import searchYouTube from '../lib/searchYouTube.js';
 import changeVideoList from './videoList.js';
 import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
-
+import ReduxThunk from 'redux-thunk';
 
 var handleVideoSearch = (q) => {
- 
+  return searchYouTube;
   //TODO:  Write an asynchronous action to handle a video search!
 };
 
